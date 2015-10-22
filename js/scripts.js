cddc877586e7b3db1a7cff699af64ac6dd047ad7
Mya Lwin & Audrey Scrugham <student@epicodus.com>
2015-10-21 18:55:16 -0700

    Pass specs for triangle types.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 var triangle = function(side1, side2, side3){
   if (side1 <= (side2 + side3) && side2 <= (side1 + side3) && side3 <= (side1 + side2)){
-    return "This is a triangle."
+    if (side1 === side2 && side2 === side3 && side3 === side1) {
+      return "This is an equilateral triangle.";
+    }else if (side1 === side2 || side1 === side3 || side2 === side3) {
+      return "This is an isosceles triangle.";
+    } else {
+      return "This is a scalene triangle.";
+    }
   } else {
-    return "These sides do not make a triangle."
+    return "These sides do not make a triangle.";
   }
-}
+};
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,14 @@ describe("triangle" , function() {
   it("determines if 3 sides create a valid triangle", function() {
     expect(triangle(2,2,8)).to.equal("These sides do not make a triangle.")
   });
+
+  it("determines if 3 sides make an equilateral triangle", function(){
+    expect(triangle(2,2,2)).to.equal("This is an equilateral triangle.")
+  });
+  it("determines if 3 sides make an isosceles triangle", function(){
+    expect(triangle(4,4,2)).to.equal("This is an isosceles triangle.")
+  });
+  it("determines if 3 sides make a scalene triangle", function(){
+    expect(triangle(4,3,2)).to.equal("This is a scalene triangle.")
+  });
 });
